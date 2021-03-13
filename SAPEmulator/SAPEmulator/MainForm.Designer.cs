b90fa37ba584a5bcb98ec87c1aeeb13a5fb87ed1
Sam Farah <osama_f2005@hotmail.com>
2021-03-12 19:44:47 -0500

    main form

--- SAPEmulator/SAPEmulator/MainForm.Designer.cs
+++ SAPEmulator/SAPEmulator/MainForm.Designer.cs
@@ -28,40 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.OpenSap1Btn = new System.Windows.Forms.Button();
             this.OpenSap2Btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // OpenSap1Btn
             // 
-            this.OpenSap1Btn.Location = new System.Drawing.Point(64, 49);
+            this.OpenSap1Btn.Location = new System.Drawing.Point(6, 128);
             this.OpenSap1Btn.Name = "OpenSap1Btn";
-            this.OpenSap1Btn.Size = new System.Drawing.Size(75, 23);
+            this.OpenSap1Btn.Size = new System.Drawing.Size(262, 23);
             this.OpenSap1Btn.TabIndex = 0;
-            this.OpenSap1Btn.Text = "SAP 1";
+            this.OpenSap1Btn.Text = "Start SAP 1";
             this.OpenSap1Btn.UseVisualStyleBackColor = true;
             this.OpenSap1Btn.Click += new System.EventHandler(this.OpenSap1Btn_Click);
             // 
             // OpenSap2Btn
             // 
-            this.OpenSap2Btn.Location = new System.Drawing.Point(64, 79);
+            this.OpenSap2Btn.Location = new System.Drawing.Point(6, 128);
             this.OpenSap2Btn.Name = "OpenSap2Btn";
-            this.OpenSap2Btn.Size = new System.Drawing.Size(75, 23);
+            this.OpenSap2Btn.Size = new System.Drawing.Size(262, 23);
             this.OpenSap2Btn.TabIndex = 1;
-            this.OpenSap2Btn.Text = "SAP 2";
+            this.OpenSap2Btn.Text = "Start SAP 2";
             this.OpenSap2Btn.UseVisualStyleBackColor = true;
             this.OpenSap2Btn.Click += new System.EventHandler(this.OpenSap2Btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.OpenSap1Btn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 159);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SAP 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.OpenSap2Btn);
+            this.groupBox2.Location = new System.Drawing.Point(292, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(274, 159);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SAP 2";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 109);
+            this.label1.TabIndex = 4;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 109);
+            this.label2.TabIndex = 5;
+            this.label2.Text = resources.GetString("label2.Text");
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 177);
-            this.Controls.Add(this.OpenSap2Btn);
-            this.Controls.Add(this.OpenSap1Btn);
+            this.ClientSize = new System.Drawing.Size(572, 180);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,5 +121,9 @@
 
         private System.Windows.Forms.Button OpenSap1Btn;
         private System.Windows.Forms.Button OpenSap2Btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
